311846f5e405d82c7b831c6a474dc21569d1cdea
haite4 <ugrayvcsgo137@gmail.com>
2025-07-12 15:34:25 +0100

    Add test.only for test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,7 @@
 import { test } from "../fixtures/testFixtures";
 import { expect } from "@playwright/test";
+import { readFileSync } from "fs";
+import path from "path";
 
 test.describe("Verify login functionality", () => {
 
@@ -7,7 +9,7 @@ test.describe("Verify login functionality", () => {
         await signinPage.open();
     });
 
-    test("Verify login on page", async({ signinPage, inventoryItemPage }) => {
+    test.only("Verify login on page", async({ signinPage, inventoryItemPage }) => {
         
         await test.step("Login", async() => {
             await signinPage.login(process.env.USER_NAME, process.env.PASSWORD);
@@ -18,13 +20,30 @@ test.describe("Verify login functionality", () => {
         });
     });
 
-    test("Verify login with incorrect data", async({ signinPage }) => {
+    test.only("Verify login with incorrect data", async({ signinPage }) => {
         await test.step("Login", async() => {
             await signinPage.login(process.env.USER_NAME, process.env.INCORRECT_PASSWORD);
         });
 
         await test.step("Verify error message is displayed.", async() => {
             await expect(signinPage.getErrorMsg()).toHaveText("Epic sadface: Username and password do not match any user in this service");
+        });
+    });
+
+    test("Verify login with parsed data", async({ signinPage }) => {
+
+        const file = readFileSync(path.resolve("data", "JupyterHubServer 2.txt"), { encoding: "utf-8"});
+
+        const machineNameValue = file.match(/Machine name:\s*(.+)/)
+        const usernameValue = file.match(/Username:\s*(.*)/)
+        const passwordValue = file.match(/Password:\s*(.*)/)
+
+        console.log(machineNameValue[1]);
+        console.log(usernameValue[1]);
+        console.log(passwordValue[1]);
+
+        await test.step("Login", async() => {
+            await signinPage.login(process.env.USER_NAME, process.env.INCORRECT_PASSWORD);
         });
     });
 });
